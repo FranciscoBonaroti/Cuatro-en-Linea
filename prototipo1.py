9a03a70f02ed09fe9799787bb69ca3f3bd3a0bf1
Francisco Bonaroti <franchu.bonaroti@gmail.com>
2021-06-20 20:33:01 -0300

    Filas y columnas agregadas

--- prototipo1.py
+++ prototipo1.py
@@ -19,3 +19,14 @@ def ContenidoFila (nrofila, tablero):
     if nrofila == 0:
         nrofila = 1
     return tablero[nrofila - 1]
+def Filardas (tablero):
+    filas = []
+    for c in range(6):
+        filas += ContenidoFila(c+1, tablero)
+    return filas
+
+def Columnardas (tablero):
+    columnas = []
+    for x in range(7):
+        columnas += ContenidoColumna (x+1, tablero)
+    return columnas
